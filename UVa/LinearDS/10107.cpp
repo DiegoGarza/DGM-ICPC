336fa532689c3de54e8cb1f6145dc860d2f5dd87
DiegoGarza <dgarza.m93@gmail.com>
2015-07-12 23:00:11 -0500

    Commit

---
+++ UVa/LinearDS/10107.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <cctype>
+#include <stack>
+#include <queue>
+#include <list>
+#include <vector>
+#include <map>
+#include <sstream>
+#include <cmath>
+#include <bitset>
+#include <utility>
+#include <set>
+#include <numeric>
+#include <ctime>
+
+#define F(i, a) for( int i = (0); i < (a); i++ )
+#define Fs(i, sz) for( size_t i = 0; i < sz.size (); i++ )
+#define Fe(i, x) for(typeof (x.begin()) i = x.begin(); i != x.end (); i++)
+#define Set(a, s) memset(a, s, sizeof (a))
+#define Max(a, b)  (a < b ? b : a)
+#define Min(a, b)  (a > b ? b : a)
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<pii> vii;
+typedef vector<vi> vvi;
+typedef map<int,int> mpii;
+typedef set<int> seti;
+
+int main(){
+    // FILE *in, *out;
+    // in = fopen("input.txt", "r");
+    // out = fopen("output.txt", "w+");
+
+    //STARTS
+    int in;
+    vector<int> v;
+    while(scanf("%d", &in) == 1){
+        v.push_back(in);
+        int m = v.size()/2;
+        sort(v.begin(), v.end());
+        v.size()%2==0?printf("%d\n", (v[m] + v[m-1])/2):printf("%d\n", v[m]);
+    }
+    //END
+
+    // fclose(in);
+    // fclose(out);
+    return 0;
+}
