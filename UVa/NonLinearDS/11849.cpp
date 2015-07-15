bb7bc6fe94c18cf08e5df930fcc562db15171613
DiegoGarza <dgarza.m93@gmail.com>
2015-07-15 11:48:04 -0500

    Solved 11849 CD

---
+++ UVa/NonLinearDS/11849.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <cctype>
+#include <stack>
+#include <queue>
+#include <list>
+#include <vector>
+#include <map>
+#include <sstream>
+#include <cmath>
+#include <bitset>
+#include <utility>
+#include <set>
+#include <numeric>
+#include <ctime>
+#include <iterator>
+#include <fstream>
+
+#define F(i, a) for( int i = (0); i < (a); i++ )
+#define Fs(i, sz) for( size_t i = 0; i < sz.size (); i++ )
+#define Fe(i, x) for(typeof (x.begin()) i = x.begin(); i != x.end (); i++)
+#define Set(a, s) memset(a, s, sizeof (a))
+#define Max(a, b)  (a < b ? b : a)
+#define Min(a, b)  (a > b ? b : a)
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<pii> vii;
+typedef vector<vi> vvi;
+typedef map<int,int> mpii;
+typedef set<int> seti;
+
+int main(){
+    // FILE *in, *out;
+    // in = fopen("input.txt", "r");
+    // out = fopen("output.txt", "w+");
+
+    //STARTS
+    long long n, m, in, total;
+    set<long long> s;
+    while(scanf("%lld%lld", &n, &m), (n || m)){
+        total = 0;
+        while(n--){
+            scanf("%lld", &in);
+            s.insert(in);
+        }
+        while(m--){
+            scanf("%lld", &in);
+            if(s.find(in) != s.end()) total++;
+        }
+        printf("%lld\n", total);
+        s.clear();
+    }
+    //END
+
+    // fclose(in);
+    // fclose(out);
+    return 0;
+}
