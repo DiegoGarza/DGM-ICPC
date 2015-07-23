096a7931c8d670496bce72e492ceb3a2bfca8a7f
DiegoGarza <dgarza.m93@gmail.com>
2015-07-23 08:47:12 -0500

    Solved

---
+++ UVa/LinearDS/11173.cpp
@@ -0,0 +1,14 @@
+#include <cstdio>
+
+int main(){
+    //STARTS
+    int cases;
+    scanf("%d", &cases);
+    while(cases--){
+        int n, k;
+        scanf("%d%d", &n, &k);
+        printf("%d\n", k^(k>>1));
+    }
+    //END
+    return 0;
+}
